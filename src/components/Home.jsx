da64b668b2e1c458fe915c34da3260e87c54514e
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-05-17 13:00:16 +0530

    Font change

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,8 @@ import {useState} from 'react'
 
 
 function Home() {
+
+    
     const[click, setClick] = useState(" Where Nature Meets Innovation");
     const handleChange=()=>{
         setClick("I am Tonmoy, Welcome to our page");
@@ -12,7 +14,8 @@ function Home() {
         <>
             
             <div className="flex flex-col items-center justify-center h-screen bg-green-100 bg">
-                <h1 className="text-4xl font-bold">Welcome to Wild Agriculture</h1>
+                <h1 className="text-4xl flex mx-auto px-4 font-bold mb-5">Welcome</h1>
+                <h2 className='text-4xl font-bold mx-auto px-4 flex'>To Wild Agriculture</h2>
                 
                 <button onClick ={handleChange} className="mt-4 bg-blue-700 text-white px-4 py-2 rounded">{click}</button>
                 
