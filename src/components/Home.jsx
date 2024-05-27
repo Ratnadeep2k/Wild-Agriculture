0ae834dbeda3e67389ac8ee86519dfe0baa32050
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-05-28 00:13:14 +0530

    "Updated Hero component: changed background image URL and title case of 'Welcome to Wild Agriculture' to uppercase."

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,12 +8,11 @@ function Hero() {
   };
 
   return (
-    <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage:  "url('https://scontent.fgau4-1.fna.fbcdn.net/v/t39.30808-6/271655235_4519768318121463_1902094322495835663_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gp-T-vqSwY0Q7kNvgF88Ygm&_nc_ht=scontent.fgau4-1.fna&cb_e2o_trans=t&oh=00_AYAwk-duyIlcQKuQCtF_LcVZlAkR8aLOgo6wsPbW-fKbDg&oe=665AB1D4')" }}>
+    <div className="relative h-screen bg-cover bg-center" style={{ backgroundImage:  "url('https://images.unsplash.com/photo-1716404084927-176ee37a9dbe?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')" }}>
       <div className="absolute inset-0 bg-black opacity-50"></div>
       <div className="container mx-auto flex flex-col items-center justify-center h-full relative z-10 px-4 text-center">
         <h1 className="text-4xl md:text-6xl font-bold text-white leading-tight">
-          Welcome to 
-        Wild Agriculture
+          WELCOME TO WILD AGRICULTURE
         </h1>
         <p className="text-lg md:text-2xl text-gray-300 mt-4">
           Sustainable farming for a better future.
