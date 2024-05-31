bc378cd32bf16db0233474c87aabc2dcb09c7d34
ratnadeep2k <55341578+Ratnadeep2k@users.noreply.github.com>
2024-06-01 00:30:41 +0530

    chore: Update firebase configuration to use external file
    - Move firebase configuration to a separate file 'firebaseConfig.js'
    - Import firebase configuration from 'firebaseConfig.js' in 'firebase.js'
    - Update '.gitignore' to exclude 'firebaseConfig.js'

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+ const firebaseConfig = {
+    apiKey: "AIzaSyCSDkMncsHs-8RxWW80g2OZB0a07qLRDL0",
+    authDomain: "wild-agriculture-a120c.firebaseapp.com",
+    projectId: "wild-agriculture-a120c",
+    storageBucket: "wild-agriculture-a120c.appspot.com",
+    messagingSenderId: "897206796254",
+    appId: "1:897206796254:web:b86cfa32a18091f846e74f",
+    measurementId: "G-RS9Q4G3786"
+  };
+  export default firebaseConfig;
--- src/firebase.js
+++ src/firebase.js
@@ -3,18 +3,8 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCSDkMncsHs-8RxWW80g2OZB0a07qLRDL0",
-  authDomain: "wild-agriculture-a120c.firebaseapp.com",
-  projectId: "wild-agriculture-a120c",
-  storageBucket: "wild-agriculture-a120c.appspot.com",
-  messagingSenderId: "897206796254",
-  appId: "1:897206796254:web:b86cfa32a18091f846e74f",
-  measurementId: "G-RS9Q4G3786"
-};
-
+import firebaseConfig from "../firebaseConfig";
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
